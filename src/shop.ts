0f680fe4deb1aad4c86a2b586f1459b93e5dcc9b
crummy <crummynz@gmail.com>
2020-04-17 16:02:50 +1200

    Fix typo

--- src/shop.ts
+++ src/shop.ts
@@ -65,7 +65,7 @@ const toGood = (row: Array<string | Number>): Good => {
 const toShippingCosts = (row: Array<string | Number>): ShippingCosts => {
   return {
     name: String(row[0]),
-    price: Number(row[1]),
+    pricePerBox: Number(row[1]),
     kgPerBox: Number(row[2])
   }
 }
